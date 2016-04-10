2bed6008a0f1d349c784ec8bcd52f262d7b009d8
jmkuehn <kuehn.51@osu.edu>
2016-04-10 05:31:22 -0400

    removed unnecessary fields in json file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var events = "https://raw.githubusercontent.com/jmkuehn/ACMWsite/js-events/events/2016.json";
+$.getJSON(events)
+  .done(function( data ) {
+    console.log(data);
+    })
+  .fail( function( ) {
+    console.log("ping");
+    });
