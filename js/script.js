d9c934d8e327cac794e922565d0c4cc6dda2ea80
jmkuehn <kuehn.51@osu.edu>
2016-04-13 23:18:17 -0400

    parsing of JSON working nicely -- changing date style to accomodate 'smart' dates

--- js/script.js
+++ js/script.js
@@ -1,31 +1,27 @@
+var icons = ["",
+  " <i class='fa fa-external-link-square'></i>",
+  " <i class='fa fa-facebook-square'></i>"];
+var mapLinkIcon= " <i class='fa fa-map-marker'></i>";
+var startYear=2016;
 var events = "https://raw.githubusercontent.com/jmkuehn/ACMWsite/js-events/events/";
-/*for(var year = 2015; year <= 2016; year++){
-  for(var month = 1; month <= 12; month++){
+
+//From startYear to current year, look for any files [monthnumber].json
+for(var year = startYear; year <= (new Date).getUTCFullYear(); year++){ 
+  for(var month = 4; month <= 4; month++){
     $.getJSON(events+year+'/'+month+'.json')
-      .done(function(data){
-        for (var event in events){
-          console.log(newEvent(event));
-        }
+      .done(function(data){//On successful import populate the page
+        populateEvents(data);
       })
-      .fail(function(a, b, c){
+      .fail(function(a, b, c){ //When fail contact me or link to github to submit pull request
         console.log(c);
       });
   }
 }
-*/
-$.getJSON(events+'2016/4.json')
-      .done(function(data){
-        for (var event in events){
-          console.log(newEvent(event));
-        }
-      })
-      .fail(function(a, b, c){
-        console.log(c);
-      });
-function errorMessage(){
-  console.log('fuck');
+function populateEvents(EventsJSON){
+  for (var i=0; i < EventsJSON.length; i++){  //Loop through events in that month
+    document.getElementsByClassName('events_container')[0].appendChild(newEvent(EventsJSON[i]));
+  }
 }
-
 function newEvent(EventJSON){
   var event = document.createElement("div");
   event.setAttribute('class', 'event');
@@ -39,42 +35,39 @@ function newEvent(EventJSON){
   date.innerHTML= EventJSON['date']
   if(EventJSON['time']){date.innerHTML+= ' | ' + EventJSON['time']}; 
   if(EventJSON['location']){
-    var location = EventJSON['location_url'] ? 
-    link(EventJSON['location_url'], EventJSON['location'], mapLinkIcon):
-    EventJSON['location'];
-    date.innerHTML+= ' | ' + location;
+    var location = EventJSON['location'];
+    date.innerHTML+= ' | ';
+    if(EventJSON['location_url']){
+      date.appendChild(link(EventJSON['location_url'], location, mapLinkIcon));
+    } else {
+      date.innerHTML+=location;
+    }
   }
   event.appendChild(date);
 
   var desc = document.createElement("h3");
   desc.setAttribute('class', 'desc');
   desc.innerHTML = EventJSON['desc'];
+  if(EventJSON['rsvp']){
+    desc.innerHTML+=  "<br/>"
+    desc.appendChild(link(EventJSON['rsvp_link'], EventJSON['rsvp'], icons[0]));
+  }
   event.appendChild(desc);
 
-  var RSVP = link(EventJSON['rsvp_url'], EventJSON['rsvp']);
-  event.appendChild(RSVP);
+  return event;
 }
 
-link = function(text){
+link = function(url, text, icon){
   var anchor = document.createElement("a");
   anchor.innerHTML = text;
-  return anchor;
-}
-link = function(url, text){
-  var anchor = link(text);
-  anchor.setAttribute('href', url);
-  anchor.setAttribute('target', '_blank');
-  return anchor;
-}
-link = function(url, text, icon){
-  var anchor = link(url, text);
+  if(url){
+    anchor.setAttribute('href', url);
+    anchor.setAttribute('target', '_blank');
+  }
   anchor.innerHTML += icon;
   return anchor;
 }
-var icons = ["", extLinkIcon, fbLinkIcon];
-var fbLinkIcon = "<i class='fa fa-facebook-square'></i>";
-var extLinkIcon= "<i class='fa fa-external-link'></i>";
-var mapLinkIcon= "<i class='fa fa-map-marker'></i>";
+
 /*
 <div class="event">
     <a class="name" href="http://www.cura.osu.edu/rsvptolle" target="_blank">
