9a70e03951865aeee341c5e8ac00db708c83a4d7
jmkuehn <kuehn.51@osu.edu>
2016-04-13 23:46:32 -0400

    reverted to old date scheme... and smart dates should work

--- js/script.js
+++ js/script.js
@@ -10,16 +10,24 @@ for(var year = startYear; year <= (new Date).getUTCFullYear(); year++){
   for(var month = 4; month <= 4; month++){
     $.getJSON(events+year+'/'+month+'.json')
       .done(function(data){//On successful import populate the page
-        populateEvents(data);
+        populateEvents(data, year);
       })
       .fail(function(a, b, c){ //When fail contact me or link to github to submit pull request
         console.log(c);
       });
   }
 }
-function populateEvents(EventsJSON){
+function populateEvents(EventsJSON, year){
   for (var i=0; i < EventsJSON.length; i++){  //Loop through events in that month
-    document.getElementsByClassName('events_container')[0].appendChild(newEvent(EventsJSON[i]));
+    var eventElement = newEvent(EventsJSON[i])
+    var eventDate = new Date(EventsJSON[i]['date']+', '+year);
+    var upcoming = document.getElementById('upcoming');
+    var past = document.getElementById('past');
+    if(eventDate > new Date){
+      upcoming.appendChild(eventElement);
+    } else {
+      past.insertBefore(eventElement, past.firstChild);
+    }
   }
 }
 function newEvent(EventJSON){
@@ -32,7 +40,7 @@ function newEvent(EventJSON){
 
   var date = document.createElement("h3");
   date.setAttribute('class', 'date');
-  date.innerHTML= EventJSON['date']
+  date.innerHTML= EventJSON['date'];
   if(EventJSON['time']){date.innerHTML+= ' | ' + EventJSON['time']}; 
   if(EventJSON['location']){
     var location = EventJSON['location'];
@@ -68,12 +76,6 @@ link = function(url, text, icon){
   return anchor;
 }
 
-/*
-<div class="event">
-    <a class="name" href="http://www.cura.osu.edu/rsvptolle" target="_blank">
-    Collaborative analytics: Meeting global challenges through shared research and development</a>
-    <h3 class="date">April 19th | 4:00-5:00pm | 
-    <a href="https://goo.gl/maps/bwsSxh9xKno" target="_blank">Wexner Center for the Arts <i class="fa fa-map-marker"></i></a></h3>
-    <h3 class="desc">This talk will focus on research collaborations that cut across disciplinary, organizational, and geographical boundaries to generate research and development breakthroughs on global challenges such as healthcare and climate change. Pushing the state of the art can be facilitated through these partnerships--public/private and academic/industry--when each group bring their unique strengths and perspectives to bear on scientific problems.</br>
-    <a href="http://www.cura.osu.edu/rsvptolle" target="_blank">RSVP</a></h3>
-</div> */
+displayDate = function(MDY){
+  return MDY;
+}
